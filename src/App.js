a3e580074fd323c90f27b0da5e8acdca2c81b237
fatlumlatifi05 <fatlumlatifi05@gmail.com>
2021-11-30 12:40:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> lumi.
+       Lumiii <code>src/App.js</code> lumi.
         </p>
         <a
           className="App-link"
