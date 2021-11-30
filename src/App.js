c1f54d19ac820ac1f2517ec9d3d6004125bcc735
Fatlum Latifi <fatlumlatifi05@gmail.com>
2021-11-30 13:41:33 +0100

    file i edituar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Fatlumi.
+          Edit <code>src/App.js</code> Fatlumi test 4.
         </p>
         <a
           className="App-link"
